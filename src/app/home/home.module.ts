034c84e5d1bf6e14edfa120391a4ade0342214df
NoumanBinTahir <rananouman785@gmail.com>
2022-10-22 22:37:34 +0500

    new modal info

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,12 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { SwiperModule } from 'swiper/angular';
 import { HomePage } from './home.page';
 import { StatsModalComponent } from '../stats-modal/stats-modal.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule, SwiperModule],
   declarations: [HomePage, StatsModalComponent],
 })
 export class HomePageModule {}
